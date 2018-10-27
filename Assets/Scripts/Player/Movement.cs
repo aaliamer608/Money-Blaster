1bb2cb96e5faab416b9ad2b17dbdf1a91d1200b5
aaliamer608 <aaliamer608@gmail.com>
2018-10-27 15:10:06 -0400

    Add files via upload
    Includes newly made "shots" prefab and scripts

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -9,30 +9,46 @@ public class Boundary
 }
 
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
     public float speed;
     public Boundary boundary;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireDelta = 0.5F;
+
     private Rigidbody rbd;
+    private float nextFire = .25f;
+    private float fireRate = 0.0f;
 
     private void Start()
     {
-        rbd = GetComponent<Rigidbody> ();
+        rbd = GetComponent<Rigidbody>();
+    }
+
+    void update()
+    {
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                GameObject clonedShot = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+            }
     }
 
     void FixedUpdate()
     {
-      //  float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(0.0f, moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         rbd.velocity = (movement * speed);
 
-        GetComponent<Rigidbody>().position = new Vector3 (
-            Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-            
-            Mathf.Clamp (GetComponent<Rigidbody>().position.y, boundary.yMin, boundary.yMax));
+        GetComponent<Rigidbody>().position = new Vector2(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+
+            Mathf.Clamp(GetComponent<Rigidbody>().position.y, boundary.yMin, boundary.yMax));
     }
 }
